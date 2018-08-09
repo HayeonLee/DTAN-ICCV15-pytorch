6013bfda8aafc88cf6743a530913161c318860a5
HayeonLee <yeonhi926@gmail.com>
2018-08-09 11:44:53 +0900

    second commit

--- preprocessing/face_alignment.py
+++ preprocessing/face_alignment.py
@@ -15,17 +15,20 @@ import os
 import csv
 import pickle
 
-def search_files(data_path, db_name, detector, fa):
+def search_files(data_path, db_name, detector, fa, new_name):
     try:
         filenames = sorted(os.listdir(data_path))
         for ith, filename in enumerate(filenames): #Emotion/S005
             full_filename = os.path.join(data_path, filename)
             if os.path.isdir(full_filename):
-               if not os.path.exists(full_filename.replace(db_name, db_name + '_pro9')):
-                  os.makedirs(full_filename.replace(db_name, db_name + '_pro9'))
-               search_files(full_filename, db_name, detector, fa)
+               if not os.path.exists(full_filename.replace(db_name, new_name)):
+                  os.makedirs(full_filename.replace(db_name, new_name))
+               search_files(full_filename, db_name, detector, fa, new_name)
             else:
               if ith in [0, int(len(filenames)/2), int(len(filenames) - 1)]:
+                print(full_filename)
+                if 'Thumbs.db' in full_filename:
+                    continue
                 image = cv2.imread(full_filename)
                 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -39,23 +42,26 @@ def search_files(data_path, db_name, detector, fa):
                   faceAligned = fa.align(image, gray, rect)
                   faceAligned = faceAligned[35:256-31, 33:256-33]
 
-                  cv2.imwrite(full_filename.replace(db_name, db_name + '_pro9'), faceAligned)
-                  print('saved')
+                  cv2.imwrite(full_filename.replace(db_name, new_name), faceAligned)
+                  #print('saved')
 
     except PermissionError:
         pass
 
-data_path = '/data/cohn-kanade-images'
-db_name = 'cohn-kanade-images'
-new_path = data_path.replace(db_name, db_name + '_processed')
+#data_path = 'data/cohn-kanade-images'
+#db_name = 'cohn-kanade-images'
+#new_path = data_path.replace(db_name, 'ck_align')
+data_path = '/data/OriginalImg/VL/Strong'
+db_name = 'OriginalImg/VL/Strong'
+new_name = 'oulu_align'
 
 if not os.path.exists(new_path):
-  os.makedirs(new_path)
+  os.makedirs(data_path.replace(db_name, new_name))
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor('preprocessing/shape_predictor_68_face_landmarks.dat')
 fa = FaceAligner(predictor, desiredFaceWidth=256)
 
-search_files(data_path, db_name, detector, fa)
+search_files(data_path, db_name, detector, fa, new_path)
 
 print('finished')
